193f8cfbcbe52914179a39656f765467179d0716
Joel Margosian <margosij@gmail.com>
2019-09-14 11:45:57 -0400

    trying to add questions to inserted paragraph

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 var number = 3;
 var intervalId;
 
-$("#start").on("click", run);
+$("#start").on("click", loadPageTwo);
 
 function run() {
     clearInterval(intervalId);
@@ -26,26 +26,33 @@ function decrement() {
     }
 
 }
-
+$("#pageTwo").html(triviaQuestions).hide();
 function stop() {
     clearInterval(intervalId);
   };
-
+//loadPageTwo function
+function loadPageTwo() {
+  //show the questions and answers
+    //on the pageTwo div, add paragraphs for each question
+    let $pageTwo= $("#pageTwo")[0]
+    console.log($pageTwo)
+    
+    for (var i=0; i<triviaQuestions.length; i++){
+      displayQuestion(triviaQuestions[i], $pageTwo)
+    }
+    //on each paragraph, add radio button answers for each question
+  //show timer
+  run()
+}
 //testing out the hide/show functions based on various scenarios
 function startFunction() {
     $("#pageTwo").show();
     $("#pageThree").hide();
-    $("#pageOne").hide();
     $("#start").hide();
   }
 
-
-
-
-
 //these are the questions
 
-$("#pageTwo").html(triviaQuestions).hide();
   var triviaQuestions = [
     {
         question: "What is the name of the actress who plays Hermione Granger in the Harry Potter series of films?",
@@ -77,47 +84,52 @@ $("#pageTwo").html(triviaQuestions).hide();
 
         correctAnswer: "2001"
     },
-
 ];
 
 
 
-function Trivia(questions, triviaContainer, resultsContainer){
-    function displayQuestion(questions, triviaContainer){
+// function Trivia(questions, triviaContainer, resultsContainer){
+    function displayQuestion(question, triviaContainer){ 
+      // console.log("trying it out")
+      // console.log(question)
+      console.log(triviaContainer)
+      let paragraph = $("<p>").text('test');
+      // paragraph.text("taco")
+      console.log(paragraph)
+      triviaContainer.append(paragraph);
+      // var output = [];
+      // var answers = [];
+  
+  
+      //setting up each question and adding pushes for the answers
+  //     for(var i=0; i<question.length; i++){
+  //         // answers = []
+  
+  //         for (var i=0; i<answers.length; i++){
+  //             answers.push(
+  //             '<input type="radio" name="answers" value="answers"></input>'
+  //                 // questions[i].answers[i]
+                  
+  //             );
+  //         }
+  // console.log(answers);
+  //       };
+
 
     }
 
     function displayResults(questions, triviaContainer, resultsContainer){
 
-    }
+    };
 
-    displayQuestion(questions, triviaContainer);
+    // output.push(
+    //        <div class="question">questions[i].question</div>
+    //         <div class="answers">answers.join("")</div>
 
-    var output = [];
-    var answers = [];
+    // triviaContainer.innerHTML = output.join("")
 
+    //)
 
-    //setting up each question and adding pushes for the answers
-    for(var i=0; i<question.length; i++){
-        answers = []
-
-        for (var i=0; i<answers.length; i++){
-            answers.prepend(
-            <input type="radio" name="answers" value="answers"></input>,
-                questions[i].answers[i]
-                
-            );
-        }
-
-      };
-
-    output.push(
-           <div class="question">questions[i].question</div>
-            <div class="answers">answers.join("")</div>
-
-    triviaContainer.innerHTML = output.join("")
-
-    )};
 
 //compare whatever answer was clicked on, compare the value of the radio button to the actual answer of the question
 //how do you get the value of the radio button?
